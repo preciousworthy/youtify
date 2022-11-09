de98e805a0facf52af860b249322149b34a9f0e1
Precious Worthy <preciousworthy@gmail.com>
2022-11-09 11:31:55 -0800

    spotify oauth authorization and GetPlaylist functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Students from "./components/students";
+import SpotifyAuth from "./components/spotifyauth";
 
 function App() {
   return (
     <div className="App">
       Hello from Techtonica
+      <SpotifyAuth />
       <Students />
     </div>
   );
