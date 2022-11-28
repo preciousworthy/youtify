09051d08586132a940f444dd31b5a234904e4751
Precious Worthy <preciousworthy@gmail.com>
2022-11-28 01:12:51 -0800

    added login page to pages

---
+++ client/src/pages/login.js
@@ -0,0 +1,10 @@
+import { SpotifyLogin } from "../components/spotify-login";
+
+const Login = () => {
+    return (
+        <div className="container text-center">
+        <SpotifyLogin />
+        </div>
+    )
+}
+export default Login;
